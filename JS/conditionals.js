e3ca124d094f92f5fb06d83829a0ec1e3caecdd9
alyxandriascott <alyxan.hope26@gmail.com>
2018-01-29 16:20:56 -0600

    update 1/29*

--- JS/conditionals.js
+++ JS/conditionals.js
@@ -188,3 +188,50 @@ function discountHandler(luckyNumber, totalAmount) {
 var luckyNumber = Math.floor(Math.random() * 5);
 
 discountHandler(luckyNumber, 100);
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+
+// ================================= CONDITIONALS BONUSES
+//
+// Bonus 1.
+//
+// Write a function (or multiple functions) that will return
+// a boolean depending on if the string value passed to it
+// as an argument is the name of a day of the week that starts with a T.
+//     Otherwise, it should return false.
+//
+//     Example: dayOfTheWeekStartsWithT("Monday")     => returns false
+// Example: dayOfTheWeekStartsWithT("Tuesday")     => returns true
+// Example: dayOfTheWeekStartsWithT("Tommy")     => returns false
+
+function tDaysOfTheWeek(input) {
+    if (input === "Tuesday" || "Thursday") {
+
+    } else {
+        alert("Doesn't begin with T");
+    }
+}
+
+console.log(tDaysOfTheWeek("Monday"));
+
+//***CHECK AS TO WHY THE ABOVE IS RETURNING UNDEFINED ^^^***
+
+
+// Bonus 2.
+//
+// Write a function, isValidPassword, that takes in a string argument and returns true or false
+// depending on whether or not all the following conditions are true:
+//
+// 1) Must be 6 characters long (only for the sake of this exercise, NOT a best practice)
+// 2) Contains at least one letter and one number
+// 3) Contains at least one upper and one lower case letter
+// 4) Only comprised of letters and numbers
+// 5) EXTRA BONUS: is not the same forwards and backwards
+
+function isValidPassword(input) {
+
+}
