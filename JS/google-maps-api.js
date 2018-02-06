db38e37b21f777872ed6b8536520ea5d896baa85
alyxandriascott <alyxan.hope26@gmail.com>
2018-02-06 15:33:14 -0600

    completed maps-api exercise and partial bonuses

--- JS/google-maps-api.js
+++ JS/google-maps-api.js
@@ -1,23 +1,36 @@
 
 "use strict";
 
+
 var address = "2606 Guadalupe St Austin, TX 78705";
 var kerbeyLane = {lat: 30.291221, lng: -97.741532 };
 var mapDisplay = document.getElementById('map');
+var geocoder;
 var options = {
-    zoom: 17,
+    zoom: 13,
     center: kerbeyLane
 };
+var map;
+
+function newMarkerFunction(place, icon) {
+    var marker = new google.maps.Marker({
+        position: place,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: icon
+    })
+}
 
 function initMap() {
-    var map = new google.maps.Map(mapDisplay, options);
+    map = new google.maps.Map(mapDisplay, options);
     var marker = new google.maps.Marker({
         position: kerbeyLane,
         map: map,
-        draggable: true
+        draggable: true,
+        animation: google.maps.Animation.DROP
     });
 
-    var geocoder = new google.maps.Geocoder();
+    geocoder = new google.maps.Geocoder();
 
     geocoder.geocode({ "address": address }, function(results, status) {
 
@@ -37,9 +50,60 @@ function initMap() {
     });
 
     var infowindow = new google.maps.InfoWindow({
-        content: "Kerbey Lane Cafe!"
+        content: ('<h4>Kerbey Lane Cafe</h4>' +
+            '<ul><li>Cinnamon Swirl Pancakes with Blueberry!!!</li><li>Chips and Queso!!</li></ul>')
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+
+    var kemuriTatsuYa = {
+        address: "2713 E 2nd St, Austin, TX 78702",
+        icon: {
+            url: "image/002-bruce-lee.svg",
+            size: new google.maps.Size(25, 25),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(45, 45)
+
+        }
+    };
+    var saltySow = {
+        address: "1917 Manor Rd, Austin, TX 78722",
+        icon: {
+            url: 'image/003-fat.svg',
+            size: new google.maps.Size(25, 25),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(45, 45)
+        }
+    };
+    var perlas = {
+        address: "1400 S Congress Ave, Austin, TX 78704",
+        icon: {
+            url: "image/001-mexican-man.svg",
+            size: new google.maps.Size(25, 25),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(45, 45)
+
+        }
+    };
+
+
+    var restaurantArray = [kemuriTatsuYa,
+        saltySow, perlas];
+
+    restaurantArray.forEach(function (e, i) {
+        geocoder.geocode({ "address": restaurantArray[i].address}, function(results, status) {
+            if(status == "OK") {
+                newMarkerFunction(results[0].geometry.location, restaurantArray[i].icon);
+            } else {
+                console.log('something went horribly, terribly, dramatically wrong XD');
+            }
+        })
     });
 
 // Open the window using our map and marker
-    infowindow.open(map, marker);
 }
