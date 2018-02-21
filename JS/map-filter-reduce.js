2bef438d01cfd6951448ab84217d65a3d7c532aa
alyxandriascott <alyxan.hope26@gmail.com>
2018-02-21 09:50:17 -0600

    update map, reduce exercise

---
+++ JS/map-filter-reduce.js
@@ -0,0 +1,43 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+
+const language = users.filter(n => n.languages.length > 2);
+console.log(language);
+
+
+const email = users.map( (n) => n.email);
+console.log(email);
+
+const reduced = users.reduce( (n, {id, name, email, languages}) => n[id] = {name, email, languages}, {} );
+console.log(reduced);
